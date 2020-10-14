7297c97fe73b6c648c6f645afa345e7ee0a3c3f3
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2020-10-14 11:43:52 -0400

    debug
    filter_taxa to prune_taxa, removes error

--- R/Functions.R
+++ R/Functions.R
@@ -1118,11 +1118,12 @@ getmode <- function(v) {
 #' run.compete()
 run.compete<-function(ps, gtab){
   require(phyloseq)
-  ps2<-filter_taxa(taxa_names(ps)!="spike1"&taxa_names(ps)!="spike2"&taxa_names(ps)!="spike3",ps)
+  ps2<-ps
+  ps2<-prune_taxa(taxa_names(ps2)!="spike1"&taxa_names(ps2)!="spike2"&taxa_names(ps2)!="spike3",ps)
   otu<-as.data.frame(as.matrix(otu_table(ps2)))
   otu<-compete(otu2, gtab)
   otu_table(ps2)<-otu_table(otu, taxa_are_rows = T)
-  ps3<-filter_taxa(taxa_names(ps)=="spike1"|taxa_names(ps)=="spike2"|taxa_names(ps)=="spike3",ps)
+  ps3<-prune_taxa(taxa_names(ps)=="spike1"|taxa_names(ps)=="spike2"|taxa_names(ps)=="spike3",ps)
   out<-merge_phyloseq(ps2, ps3)
   out # test this!!
 }
